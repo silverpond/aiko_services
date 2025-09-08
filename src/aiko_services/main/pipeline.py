9a9b84daaa987ea75008ce82df4c593f30411b3b
Rowan Skewes <rowan.skewes@silverpond.com.au>
2025-09-08 17:20:17 +1000

    Release stream lock when exception is raised in frame generator

--- src/aiko_services/main/pipeline.py
+++ src/aiko_services/main/pipeline.py
@@ -1106,6 +1106,7 @@ class PipelineImpl(Pipeline):
                 actor = get_actor_mqtt(stream.topic_response, Pipeline)
                 actor.process_frame_response(stream_info, diagnostic)
         finally:
+            stream.lock.release()
             if use_thread_local:
                 stream.lock.release()
                 self._disable_thread_local("destroy_stream()")
